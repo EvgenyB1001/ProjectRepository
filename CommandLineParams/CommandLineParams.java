7c2100d36d61ecf7410144c1b6116856039ca0ae
EvgenyB1001 <borbut1001@gmail.com>
2016-10-02 21:26:16 +0300

    The first task modification 1

--- CommandLineParams/CommandLineParams.java
+++ CommandLineParams/CommandLineParams.java
@@ -1,8 +1,8 @@
-//The program outputs parameters from the command line in reverse order
+/** The program outputs parameters from the command line in reverse order */
 public class CommandLineParams {
     public static void main(String[] args) {
         for(int i = args.length-1; i >= 0; i--) {
-            System.out.println("Аргумент " + i + " = " + args[i]);
+            System.out.println("Argument " + i + " = " + args[i]);
         }
     }
 }
