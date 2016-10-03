295263bd4ca8f9ad05b6648b939c1350113144d4
EvgenyB1001 <borbut1001@gmail.com>
2016-10-03 11:45:44 +0300

    The third task modified 3

--- task03/Calculate.java
+++ task03/Calculate.java
@@ -1,4 +1,6 @@
-/** This program does 4 math operations (+, -, *, /). Each operation has there own method. */
+/** This program does 4 math operations (+, -, *, /).
+ * Each operation has there own method.
+ */
 public class Calculate {
     /** Method returns sum of parameters
      * @param param1 the first parameter of calculation
@@ -35,12 +37,13 @@ public class Calculate {
     private static double quotient(double param1, double param2) {
         return param1 / param2;
     }
-    /** Method main gets parameters from command line, verifies parameteres and
+    /**
+     * Method main gets parameters from command line, verifies parameteres and
      * outputs results of calculate operations
      */
     public static void main(String[] args) {
         double[] numbers = new double[args.length];
-        /** It provides correct format of parameters and verifies that this is possible */
+        // It provides correct format of parameters and verifies that this is possible
         try {
             for (int i = 0; i < args.length; i++) {
                 numbers[i] = Double.parseDouble(args[i]);
@@ -52,7 +55,7 @@ public class Calculate {
         if (numbers.length >= 3) {
            System.out.println("Unnecessary parameters are ignored");
         }
-       /** Verifies that there are not missed parameters */
+       // Verifies that there are not missed parameters */
         if (numbers.length <= 1) {
                 System.out.println("Missed parameters. Expected two parameters");
                 return;
@@ -61,9 +64,7 @@ public class Calculate {
             System.out.println("a + b = " + sum(numbers[0], numbers[1]));
             System.out.println("a * b = " + multiply(numbers[0],numbers[1]));
             System.out.println("a - b = " + difference(numbers[0], numbers[1]));
-            /** If there are division by zero, it returns error. Otherwise, it returns result
-             * of the calculation
-             */
+            // If there are division by zero, it returns error. Otherwise, it returns result of the calculation
             if (Double.compare(numbers[1], 0) == 0) {
                 System.out.println("Division by zero isn't accepted");
             } else {
