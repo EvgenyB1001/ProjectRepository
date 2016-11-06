57683104757c9c62eae1b99270c23f4a125afed1
EvgenyB1001 <borbut1001@gmail.com>
2016-11-06 10:53:32 +0300

    Triangle test third commit

--- Triangle_Test/src/main/java/homework/TriangleTypeDetector.java
+++ Triangle_Test/src/main/java/homework/TriangleTypeDetector.java
@@ -13,7 +13,7 @@ public class TriangleTypeDetector {
      * @param thirdSide value of third side
      */
     public String detectTriangle(double firstSide, double secondSide, double thirdSide) throws Exception {
-        new Validation().validatesSides(firstSide, secondSide, thirdSide);
+        validatesSides(firstSide, secondSide, thirdSide);
         // Determines the type of triangle depending on the number of equal sides
         if (Double.compare(firstSide, secondSide) == 0 && Double.compare(firstSide, thirdSide) == 0
                 && Double.compare(secondSide, thirdSide) == 0) {
@@ -24,5 +24,29 @@ public class TriangleTypeDetector {
             return "Triangle is isosceles";
         }
         return "Triangle is normal";
+    }
+
+    /**
+     * Method validatesSides gets values of sides and verify that values are correct
+     * and the triangle with this sides exists. If it isn't, it throws exception
+     *
+     * @param side1 value of the first side
+     * @param side2 value of the second side
+     * @param side3 value of the third side
+     */
+    private void validatesSides(double side1, double side2, double side3) throws Exception {
+        double[] params = {side1, side2, side3};
+        // Validates values of sides
+        for (double sides : params) {
+            if (Double.compare(sides, 0) <= 0 || Double.compare(sides, Double.NaN) == 0
+                    || Double.compare(sides, Double.POSITIVE_INFINITY) == 0
+                    || Double.compare(sides, Double.NEGATIVE_INFINITY) == 0) {
+                throw new Exception("Incorrect side value");
+            }
+        }
+        // Checks that the triangle exists
+        if (params[0] >= params[1] + params[2] || params[0] <= Math.abs(params[1] - params[2])) {
+            throw new Exception("Triangle doesn't exist with such values of sides");
+        }
     }
 }
