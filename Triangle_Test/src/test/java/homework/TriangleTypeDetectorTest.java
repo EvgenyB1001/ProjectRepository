90e57f8d23b58b2ba3730ed8d3c3e581636271b4
EvgenyB1001 <borbut1001@gmail.com>
2016-11-10 16:27:13 +0300

    added ReaderXML

--- Triangle_Test/src/test/java/homework/TriangleTypeDetectorTest.java
+++ Triangle_Test/src/test/java/homework/TriangleTypeDetectorTest.java
@@ -16,13 +16,13 @@ import java.math.BigDecimal;
 public class TriangleTypeDetectorTest {
 
     TriangleTypeDetector detector;
+    ReaderXML readerXML = new ReaderXML();
+    private final String FILE_PATH = System.getProperty("user.dir") + "/Data_Provider.xml";
     private static final String ILLEGAL = "illegal";
     private static final String ISOSCELES = "isosceles";
     private static final String EQUILATERAL = "equilateral";
     private static final String NORMAL = "normal";
-    private static final String SIDE_A = "side_a";
-    private static final String SIDE_B = "side_b";
-    private static final String SIDE_C = "side_c";
+    private static final String[] SIDES = {"side_a", "side_b", "side_c"};
 
 
     @BeforeTest
@@ -32,84 +32,21 @@ public class TriangleTypeDetectorTest {
 
     @DataProvider(name = "illegal from XML")
     public Object[][] readIllegalValuesXml() throws Exception {
-        File inputFile = new File(System.getProperty("user.dir") + "/Data_Provider.xml");
-        DocumentBuilderFactory factory = DocumentBuilderFactory.newInstance();
-        DocumentBuilder builder = factory.newDocumentBuilder();
-
-        Document document = builder.parse(inputFile);
-        NodeList list = document.getElementsByTagName(ILLEGAL);
-        Object[][] result = new BigDecimal[list.getLength()][];
-        for (int i = 0; i < list.getLength(); i++) {
-            NamedNodeMap map = list.item(i).getAttributes();
-            result[i] = new BigDecimal[]{
-                    map.getNamedItem(SIDE_A) == null ?
-                            null : new BigDecimal(map.getNamedItem(SIDE_A).getNodeValue()) ,
-                    map.getNamedItem(SIDE_B) == null ?
-                            null : new BigDecimal(map.getNamedItem(SIDE_B).getNodeValue()),
-                    map.getNamedItem(SIDE_C) == null ?
-                            null : new BigDecimal(map.getNamedItem(SIDE_C).getNodeValue())
-            };
-        }
-        return result;
-        }
+        return readerXML.read(FILE_PATH, ILLEGAL, SIDES);
+    }
 
     @DataProvider(name = "isosceles from XML")
     public Object[][] readIsoscelesValuesXml() throws Exception {
-        File inputFile = new File(System.getProperty("user.dir") + "/Data_Provider.xml");
-        DocumentBuilderFactory factory = DocumentBuilderFactory.newInstance();
-        DocumentBuilder builder = factory.newDocumentBuilder();
-
-        Document document = builder.parse(inputFile);
-        NodeList list = document.getElementsByTagName(ISOSCELES);
-        Object[][] result = new BigDecimal[list.getLength()][];
-        for (int i = 0; i < list.getLength(); i++) {
-            NamedNodeMap map = list.item(i).getAttributes();
-            result[i] = new BigDecimal[]{
-                    new BigDecimal(map.getNamedItem(SIDE_A).getNodeValue()),
-                    new BigDecimal(map.getNamedItem(SIDE_B).getNodeValue()),
-                    new BigDecimal(map.getNamedItem(SIDE_C).getNodeValue())
-            };
-        }
-        return result;
+        return readerXML.read(FILE_PATH, ISOSCELES, SIDES);
     }
 
     @DataProvider(name = "normal from XML")
     public Object[][] readNormalValuesXml() throws Exception {
-        File inputFile = new File(System.getProperty("user.dir") + "/Data_Provider.xml");
-        DocumentBuilderFactory factory = DocumentBuilderFactory.newInstance();
-        DocumentBuilder builder = factory.newDocumentBuilder();
-
-        Document document = builder.parse(inputFile);
-        NodeList list = document.getElementsByTagName(NORMAL);
-        Object[][] result = new BigDecimal[list.getLength()][];
-        for (int i = 0; i < list.getLength(); i++) {
-            NamedNodeMap map = list.item(i).getAttributes();
-            result[i] = new BigDecimal[]{
-                    new BigDecimal(map.getNamedItem(SIDE_A).getNodeValue()),
-                    new BigDecimal(map.getNamedItem(SIDE_B).getNodeValue()),
-                    new BigDecimal(map.getNamedItem(SIDE_C).getNodeValue())
-            };
-        }
-        return result;
+        return readerXML.read(FILE_PATH, NORMAL, SIDES);
     }
     @DataProvider(name = "equilateral from XML")
     public Object[][] readEquilateralValuesXml() throws Exception {
-        File inputFile = new File(System.getProperty("user.dir") + "/Data_Provider.xml");
-        DocumentBuilderFactory factory = DocumentBuilderFactory.newInstance();
-        DocumentBuilder builder = factory.newDocumentBuilder();
-
-        Document document = builder.parse(inputFile);
-        NodeList list = document.getElementsByTagName(EQUILATERAL);
-        Object[][] result = new BigDecimal[list.getLength()][];
-        for (int i = 0; i < list.getLength(); i++) {
-            NamedNodeMap map = list.item(i).getAttributes();
-            result[i] = new BigDecimal[]{
-                    new BigDecimal(map.getNamedItem(SIDE_A).getNodeValue()),
-                    new BigDecimal(map.getNamedItem(SIDE_B).getNodeValue()),
-                    new BigDecimal(map.getNamedItem(SIDE_C).getNodeValue())
-            };
-        }
-        return result;
+        return readerXML.read(FILE_PATH, EQUILATERAL, SIDES);
     }
 
     @Test(dataProvider = "illegal from XML", expectedExceptions = Exception.class)
